cf0ffef9979186b1f0ade89bbb23b754c902ca30
Harriet O'Brien <42326843+harrietobrien@users.noreply.github.com>
2023-03-28 20:03:15 -0400

    square root

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,3 +1,17 @@
 #include "square_root.h"
+#include <stdint.h>
+
+int  square_root(int radicand) {
+  int sq_rt;
+  if (radicand < 2) {
+      sq_rt = radicand;
+  }
+  for (int i = 0; i <= (2 * radicand / 3); i++) {
+    if (i * i == radicand) {
+      sq_rt =  i;
+    }
+  }
+  return sq_rt;
+}
 
 
